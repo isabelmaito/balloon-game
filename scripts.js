ebb5dd4923011eb71f5c2ed4fd8b8d4e13369fd2
isabelmaito <maitoisabel@gmail.com>
2023-09-17 22:52:58 -0300

    feat: balloon controls

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
-function handleKeyDown(event){
+const balloon = document.getElementById("balloon")
+const velocidade = 5
 
+function handleKeyDown(event){
+    const computed_style = getComputedStyle(balloon)
+    const y = parseInt(computed_style.top)
+    const x = parseInt(computed_style.left)
+    if(event.key=="ArrowUp"){
+        balloon.style.top = (y-velocidade) + "px"
+    }
+    if(event.key=="ArrowDown"){
+        balloon.style.top = (y+velocidade) + "px"
+    }
+    if(event.key=="ArrowLeft"){
+        balloon.style.left = (x-velocidade) + "px"
+    }
+    if(event.key=="ArrowRight"){
+        balloon.style.left = (x+velocidade) + "px"
+    }
 }
 
 document.addEventListener("keydown",handleKeyDown)
